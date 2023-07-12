fb040f5d0ffe7d38d30a5625160d19616c8f0cd1
garyokeeffe <garyjokeeffe@gmail.com>
2023-07-12 12:41:21 -0700

    make path handling more robust

--- app/app.py
+++ app/app.py
@@ -4,10 +4,14 @@ import nostr_helper
 
 app = Flask(__name__)
 
-@app.route('/verify')
+@app.route('/verify', methods=['GET'])
 def verify_API():
     nostr_helper.verify_API()
     return jsonify(message="Success")
+
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def catch_all(path):
+    return jsonify(message=f"You've hit the {path} path")
 
 def handler(event, context):
     return serverless_wsgi.handle_request(app, event, context)
