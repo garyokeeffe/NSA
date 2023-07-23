2c550dcf0fec254d1bc016272efd40e2ea71ed0a
garyokeeffe <garyjokeeffe@gmail.com>
2023-07-22 18:38:03 -0700

    create endpoint for fetching "likes"

--- app/app.py
+++ app/app.py
@@ -27,6 +27,17 @@ def fetch_text_notes():
     except Exception as e:
         return jsonify({'error': 'An unexpected error occurred'}), 500
 
+@app.route('/v0/fetch/likes', methods=['POST'])
+def fetch_text_notes():
+    try:
+        request_data = request.get_json()
+        event_filter = nostr_helper.generate_fetch_like_filter(request_data)
+        relays = nostr_helper.find_request_relay(request_data)
+        return jsonify(nostr_helper.fetch_likes(event_filter, relays))
+    except Exception as e:
+        return jsonify({'error': 'An unexpected error occurred'}), 500
+
+
 @app.route('/v0/send/note', methods=['POST'])
 def send_text_note():
     try:
